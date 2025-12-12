1b23b69d53ad7dd6fd9adb02aed848db58a45077
Show Wai Yan <showwaiyan555@gmail.com>
2025-12-12 15:36:20 +0800

    Add onOpen and onClose callback for trigger ScannerModal

--- UI/Modals/cameraModal.ts
+++ UI/Modals/cameraModal.ts
@@ -9,4 +9,30 @@ export class ScannerModal extends Modal {
 		this.setTitle("Scan Your Note");
 	}
 
+	async onOpen() {
+		// Create video element
+		this.videoElement = this.contentEl.createEl("video");
+		// Get User Media
+		try {
+			this.stream = await navigator.mediaDevices.getUserMedia({
+				video: true,
+				audio: false,
+			});
+		} catch (error) {
+			new Notice(`Something Went Wrong.\nError: ${error.message}`);
+			return;
+		}
+		// Get the Stream
+		// Set video srcObject to that media
+	}
+
+	async onClose() {
+		// Close steam's each track
+		if (!this.stream) return;
+		const tracks = this.stream.getTracks();
+		tracks.forEach((track) => {
+			track.stop();
+		});
+		this.close();
+	}
 }
